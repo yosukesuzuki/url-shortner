4b18ddf9a77a392dba24714d348e829f961626e5
Yosuke Suzuki <yosuke.suzuki@gmail.com>
2017-07-17 15:03:47 +0900

    [wip]update models

--- models.py
+++ models.py
@@ -1,12 +1,42 @@
+import validators
+from google.appengine.api import datastore_errors
 from google.appengine.ext import ndb
 
-"""
-class Events(ndb.Model):
-    user = ndb.UserProperty()
-    alias = ndb.StringProperty()
-    attendee_list = ndb.JsonProperty()
-    attendee_combination = ndb.JsonProperty()
-    public = ndb.BooleanProperty(default=False)
+
+def fqdn_validator(prop, value):
+    if validators.domain(value) is not True:
+        raise datastore_errors.BadValueError(prop._name)
+
+
+class CustomDomain(ndb.Model):
+    fqdn = ndb.StringProperty(validator=fqdn_validator)
+    verified = ndb.BooleanProperty(default=False)
     updated_at = ndb.DateTimeProperty(auto_now=True)
     created_at = ndb.DateTimeProperty(auto_now_add=True)
-"""
+
+
+class Team(ndb.Model):
+    """
+    team_domain is the sub domain for each team
+    """
+    team_name = ndb.StringProperty(required=True)
+    billing_plan = ndb.KeyProperty(required=True)
+    primary_owner = ndb.KeyProperty(required=True)
+    team_domain = ndb.StringProperty(required=True)
+    custom_domain = ndb.StructuredProperty(CustomDomain, repeated=True)
+    updated_at = ndb.DateTimeProperty(auto_now=True)
+    created_at = ndb.DateTimeProperty(auto_now_add=True)
+
+
+class User(ndb.Model):
+    """
+    key_name = team_name + "_" + user_no
+
+    """
+    user = ndb.UserProperty()
+    user_id = ndb.StringProperty()  # user can set
+    user_name = ndb.StringProperty()  # display name
+    team = ndb.KeyProperty()
+    role = ndb.StringProperty(choices=('primary_owner', 'admin', 'normal'))
+    updated_at = ndb.DateTimeProperty(auto_now=True)
+    created_at = ndb.DateTimeProperty(auto_now_add=True)
